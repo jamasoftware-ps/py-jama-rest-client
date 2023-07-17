9009419781371afc7aefc1e144b0dbb9b146d8ff
auhallet <austin.hallett-doughty@silabs.com>
2023-07-17 10:45:22 -0400

    update version + formatting

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 # Arguments marked as "Required" below must be included for upload to PyPI.
@@ -29,21 +29,18 @@ setup(
     # There are some restrictions on what makes a valid project name
     # specification here:
     # https://packaging.python.org/specifications/core-metadata/#name
-    name='py-jama-rest-client',  # Required
-
+    name="py-jama-rest-client",  # Required
     # Versions should comply with PEP 440:
     # https://www.python.org/dev/peps/pep-0440/
     #
     # For a discussion on single-sourcing the version across setup.py and the
     # project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.17.1',  # Required
-
+    version="1.17.2b0",  # Required
     # This is a one-line description or tagline of what your project does. This
     # corresponds to the "Summary" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#summary
-    description='A client for the Jama Connect REST API',  # Optional
-
+    description="A client for the Jama Connect REST API",  # Optional
     # This is an optional longer description of your project that represents
     # the body of text which users will see when they visit PyPI.
     #
@@ -53,7 +50,6 @@ setup(
     # This field corresponds to the "Description" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-optional
     long_description=long_description,  # Optional
-
     # Denotes that our long_description is in Markdown; valid values are
     # text/plain, text/x-rst, and text/markdown
     #
@@ -64,25 +60,20 @@ setup(
     #
     # This field corresponds to the "Description-Content-Type" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#description-content-type-optional
-    long_description_content_type='text/markdown',  # Optional (see note above)
-
+    long_description_content_type="text/markdown",  # Optional (see note above)
     # This should be a valid link to your project's main homepage.
     #
     # This field corresponds to the "Home-Page" metadata field:
     # https://packaging.python.org/specifications/core-metadata/#home-page-optional
-    url='https://github.com/JamaSoftware/py-jama-rest-client',  # Optional
-
+    url="https://github.com/JamaSoftware/py-jama-rest-client",  # Optional
     # Declare license
-    license='MIT',
-
+    license="MIT",
     # This should be your name or the name of the organization which owns the
     # project.
-    author='Nick McHale',  # Optional
-
+    author="Nick McHale",  # Optional
     # This should be a valid email address corresponding to the author listed
     # above.
-    author_email='nmchale@jamasoftware.com',  # Optional
-
+    author_email="nmchale@jamasoftware.com",  # Optional
     # Classifiers help users find your project by categorizing it.
     #
     # For a list of valid classifiers, see https://pypi.org/classifiers/
@@ -91,26 +82,21 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
-
+        "Development Status :: 4 - Beta",
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Libraries',
-
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Libraries",
         # Pick your license as you wish
-        'License :: OSI Approved :: MIT License',
-
+        "License :: OSI Approved :: MIT License",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        'Programming Language :: Python :: 3.7',
+        "Programming Language :: Python :: 3.7",
     ],
-
     # This field adds keywords for your project which will appear on the
     # project page. What does your project relate to?
     #
     # Note that this is a string of words separated by whitespace, not a list.
-    keywords='REST Client',  # Optional
-
+    keywords="REST Client",  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -120,16 +106,14 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages(exclude=['contrib', 'docs', 'test']),  # Required
-
+    packages=find_packages(exclude=["contrib", "docs", "test"]),  # Required
     # This field lists other packages that your project depends on to run.
     # Any package you put here will be installed by pip when your project is
     # installed, so they must be valid existing projects.
     #
     # For an analysis of "install_requires" vs pip's requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['requests'],  # Optional
-
+    install_requires=["requests"],  # Optional
     # List additional groups of dependencies here (e.g. development
     # dependencies). Users will be able to install these using the "extras"
     # syntax, for example:
@@ -142,7 +126,6 @@ setup(
     #     'dev': ['check-manifest'],
     #     'test': ['coverage'],
     # },
-
     # If there are data files included in your packages that need to be
     # installed, specify them here.
     #
@@ -151,14 +134,12 @@ setup(
     # package_data={  # Optional
     #     'sample': ['package_data.dat'],
     # },
-
     # Although 'package_data' is the preferred approach, in some case you may
     # need to place data files outside of your packages. See:
     # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files
     # #
     # # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
     # data_files=[('my_data', ['data/data_file'])],  # Optional
-
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
     # `pip` to create the appropriate form of executable for the target
@@ -171,7 +152,6 @@ setup(
     #         'sample=sample:main',
     #     ],
     # },
-
     # List additional URLs that are relevant to your project as a dict.
     #
     # This field corresponds to the "Project-URL" metadata fields:
@@ -182,9 +162,9 @@ setup(
     # maintainers, and where to support the project financially. The key is
     # what's used to render the link text on PyPI.
     project_urls={  # Optional
-        'Bug Reports': 'https://github.com/JamaSoftware/py-jama-rest-client/issues',
-        'Source': 'https://github.com/JamaSoftware/py-jama-rest-client',
-        'Jama Software': 'https://www.jamasoftware.com',
-        'Jama Developer resources': 'https://dev.jamasoftware.com',
+        "Bug Reports": "https://github.com/JamaSoftware/py-jama-rest-client/issues",
+        "Source": "https://github.com/JamaSoftware/py-jama-rest-client",
+        "Jama Software": "https://www.jamasoftware.com",
+        "Jama Developer resources": "https://dev.jamasoftware.com",
     },
 )
